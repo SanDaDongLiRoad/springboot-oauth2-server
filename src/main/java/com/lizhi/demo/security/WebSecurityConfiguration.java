b9efc43f036d5e657ee2aef3f99bee6bbde73189
SanDaDongLiRoad <1013310985@qq.com>
2019-05-13 21:12:46 +0800

    创建角色和角色用户关联

--- src/main/java/com/lizhi/demo/security/WebSecurityConfiguration.java
+++ src/main/java/com/lizhi/demo/security/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author lenovo
@@ -23,5 +24,25 @@ public class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdap
     public void init(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService);
     }
+
+//    @Override
+//    public void init(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userService)
+//                .passwordEncoder(new PasswordEncoder() {
+//                    @Override
+//                    public String encode(CharSequence charSequence)
+//                    {
+//                        return charSequence.toString();
+//                        //return SecureUtil.md5(charSequence.toString());
+//                    }
+//
+//                    @Override
+//                    public boolean matches(CharSequence charSequence, String s) {
+//                        //return SecureUtil.md5(charSequence.toString()).equals(s);
+//                        return charSequence.toString().equals(s);
+//                    }
+//                });
+//        // auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("user1").password(new BCryptPasswordEncoder().encode("123456")).roles("USER");
+//    }
 
 }
