7e1815488f7255b53e158a1b70e2a46de65fd889
SanDaDongLiRoad <1013310985@qq.com>
2019-05-13 21:13:22 +0800

    创建角色和角色用户关联

--- src/main/java/com/lizhi/demo/config/WebMvcConfig.java
+++ src/main/java/com/lizhi/demo/config/WebMvcConfig.java
@@ -1,22 +1,22 @@
-package com.lizhi.demo.config;
-
-import com.lizhi.demo.interceptor.AccessInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-/**
- * @author lenovo
- */
-@Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
-
-    @Autowired
-    private AccessInterceptor accessInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(accessInterceptor);
-    }
-}
+//package com.lizhi.demo.config;
+//
+//import com.lizhi.demo.interceptor.AccessInterceptor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+///**
+// * @author lenovo
+// */
+//@Configuration
+//public class WebMvcConfig extends WebMvcConfigurerAdapter {
+//
+//    @Autowired
+//    private AccessInterceptor accessInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(accessInterceptor);
+//    }
+//}
