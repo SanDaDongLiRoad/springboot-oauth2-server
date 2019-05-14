46b540e4630e3cf2bacb6635203e34395cb48f28
lenovo <15001295170@163.com>
2019-05-14 09:02:02 +0800

    整理代码

--- src/main/java/com/lizhi/demo/aspect/HttpAspect.java
+++ src/main/java/com/lizhi/demo/aspect/HttpAspect.java
@@ -9,7 +9,6 @@ import com.lizhi.demo.service.RoleService;
 import com.lizhi.demo.service.UserRoleService;
 import com.lizhi.demo.service.UserService;
 import com.lizhi.demo.utils.ResultUtil;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
@@ -60,18 +59,6 @@ public class HttpAspect {
         HttpServletRequest request = attributes.getRequest();
         String token = request.getHeader("Authorization").replace("Bearer","").intern();
 
-        //url
-        logger.info("url={}", request.getRequestURL());
-        //method
-        logger.info("method={}", request.getMethod());
-        //ip
-        logger.info("ip={}", request.getRemoteAddr());
-        //类方法
-        logger.info("class_method={}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        //参数
-        logger.info("args={}", joinPoint.getArgs());
-
-
         Signature sig = joinPoint.getSignature();
         MethodSignature msig = null;
         if (!(sig instanceof MethodSignature)) {
@@ -93,18 +80,6 @@ public class HttpAspect {
                 return joinPoint.proceed();
             }
         }
-
         return ResultUtil.error(-1,"无权限");
     }
-
-    @After("log()")
-    public void doAfter() {
-        logger.info("222222222222");
-    }
-
-    @AfterReturning(returning = "object", pointcut = "log()")
-    public void doAfterReturning(Object object) {
-        logger.info("response={}", object.toString());
-    }
-
 }
